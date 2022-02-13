e70230a2d28c898fcf6a2b8034438dee6947ceca
Mike <4xgood@gmail.com>
2022-02-13 05:15:19 -0700

    read and write files

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-const amount = 12;
+const { readFile, writeFile } = require("fs");
+
+readFile("./first.txt", "utf-8", (err, result) => {
+  console.log(result);
+});
+
+writeFile("./write.txt", "write this to file", (err, result) => {
+  console.log(result);
+});
